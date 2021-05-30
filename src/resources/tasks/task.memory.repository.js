84a4c88c0a9b4d232f351aeb84877f6cc71ac4d8
mishafuod-13 <mishafuod@gmail.com>
2021-05-30 13:17:31 +0300

    docs: add js.doc comment for tasks-bd class

--- src/resources/tasks/task.memory.repository.js
+++ src/resources/tasks/task.memory.repository.js
@@ -4,13 +4,30 @@
  */
 const {BD} = require('../boards/board.memory.repositiry');
 
+/**
+ * @module Task
+ * @description imports Task- model
+ */
 const {Task} = require('./task.model');
 
 class TasksBD {
+  /**
+    * @requires BD
+    * @requires Task
+    * @this TasksBD
+     * Create a TasksBD entity.
+     * @param {object} [this.taskrep={}] -  an object with keys boardId = [Taskmodel]
+     */
     constructor() {
         this.taskrep = {};
     }
 
+    /** 
+     * Сreates a new task entity and puts it in the storage by key
+     * @param {string} boardId  - id/key 
+     * @param {object} taskoption - Object with options for new task
+      * @returns {object|Error} return new task-object or error if input values are incorrect.
+      */
     addTask (boardId, taskoption) {
       const board  = BD.findBoard(boardId);
       const task = new Task (taskoption);
@@ -26,6 +43,11 @@ class TasksBD {
       return Error ("Bad reqest");
     }
 
+    /** 
+       * Searches all task by boardId
+       * @param {string} boardId - boardId by which to search.
+       * @returns {Array|Error} - returns an array with task entities or error if boardId are incorrect/not exist.
+       */
     getTasks (boardId) {
        const board  = BD.findBoard(boardId);
        if (!board){
@@ -34,6 +56,13 @@ class TasksBD {
        return this.taskrep[boardId]
     }
 
+    /** 
+     * Update task by boardId & taskId
+     * @param {string} boardId  - id/key for this.taskrep (see above) 
+     * @param {string} taskId  - taskId
+     * @param {object} options - Object with options for updated task
+     * @returns {object|undefined} return new task-object or undefined if input values are incorrect.
+    */
     updateTask (boardId, taskId, options) {
       let result
         this.taskrep[boardId].forEach(item => {
@@ -45,6 +74,12 @@ class TasksBD {
       return result;
     }
 
+    /** 
+     * Delete task by boardId & taskId
+     * @param {string} boardId  - id/key for this.taskrep (see above) 
+     * @param {string} taskId  - taskId
+     * @returns {string|Error} - if the operation was successful, otherwise an error
+    */
     deleteTask(boardId, taskId){
       const board = this.taskrep[boardId]
       let res;
@@ -60,6 +95,12 @@ class TasksBD {
       throw Error ("Access token is missing or invalid");
     }
 
+    /** 
+     * Find & return task by boardId & taskID
+     * @param {string} boardId  - id/key for this.taskrep (see above) 
+     * @param {string} taskId  - taskId
+     * @returns {object|Error|undefined} - if the operation was successful, otherwise an error or undefined
+    */
     getTaskById (boardId, taskId) {
        const tasks = this.getTasks(boardId);
        let result; 
@@ -75,6 +116,11 @@ class TasksBD {
     }
 }
 
+/** @constant {object} TaskBD - creates an instance of the class TasksBD */
 const TaskBD = new TasksBD ();
 
+/**
+ * Export TaskBD module
+ * @module TaskBD
+ */
 module.exports.TaskBD = TaskBD;
