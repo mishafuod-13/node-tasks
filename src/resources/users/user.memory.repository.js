ea4f8f35b2f291a93677f07cb6722a636f067aa3
mishafuod-13 <mishafuod@gmail.com>
2021-05-30 15:13:15 +0300

    docs: add JSDoc comments on usersbd class

--- src/resources/users/user.memory.repository.js
+++ src/resources/users/user.memory.repository.js
@@ -1,19 +1,38 @@
+/**
+ * Import User -entity class
+ * @module User
+ */
 const UserModel = require('./user.model.js').User;
+
 const {userUpdateDelete} = require('./user.update.delete');
 
 class UsersBD {
+ /**
+    * @requires User
+    * @this UsersBD
+     * Create a UserBD entity.
+     * @param {Array} [this.users=[]] - Array with Users entities.
+     */
   constructor() {
-    this.model = UserModel;
     this.users = [];
   }
 
+  /** 
+       * Create new user - entity and puts that on "database".
+       * @param {object} options - an object with keys to create a new user.
+       * @returns {object} - return new object - user.
+       */
   createNewUser (options) {
     const newUser = new UserModel(options);
     this.users.push(newUser)
     return newUser.toResponse();
   }
 
-
+  /** 
+       * Delete user - entity.
+       * @param {string} userId - User Id.
+       * @returns {string|null} - string "OK" if user deleted, or null 
+       */
   async deleteUser( userId ) {
     const result = this.findUser(userId);
     if (result) {
@@ -24,6 +43,12 @@ class UsersBD {
     return result;
   }
 
+  /** 
+       * Update user - entity and puts that on "database".
+       * @param {string} userId - User Id.
+       * @param {object} options - an object with keys to update a user- entity.
+       * @returns {object|null} -  object user-entity if update succesfull, or null.
+       */
   updateUser ( userId, options ){
     const result = this.findUser(userId);
     if (result) {
@@ -34,6 +59,11 @@ class UsersBD {
     return result;
   }
 
+  /** 
+       * Return user-entity by Id.
+       * @param {string} userId - User Id.
+       * @returns {object|null} -  object user-entity if update succesfull, or null.
+       */
   getUser (userId) {
     const result = this.findUser(userId);
     if (result !== null){
@@ -42,6 +72,11 @@ class UsersBD {
     return result;
   }
 
+  /** 
+       * Search user-entity in "database".
+       * @param {string} userId - User Id.
+       * @returns {number|null} -  Returns the index of the match, otherwise null.
+       */
   findUser (userId){
     const result = [];
     this.users.forEach ( (user, index) => {
@@ -59,6 +94,11 @@ class UsersBD {
     return null;
   }
 
+  /** 
+       * Get all user-entities
+       * @param {string} userId - User Id.
+       * @returns {Array} -  Returns an array with all entities
+       */
   getAll () {
       return this.users
         .map(user => user.toResponse())
@@ -66,5 +106,11 @@ class UsersBD {
   
 }
 
+/** @constant {object} UserBD - creates an instance of the class UsersBD */
 const UserBD = new UsersBD();
+
+/**
+ * Export the created instance of the UsersBD class.
+ * @module UserBD 
+ */
 module.exports.UserBD = UserBD;
