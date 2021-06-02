5ee277ff641f9fbf95d5b99e53f398de4296d92b
mishafuod-13 <mishafuod@gmail.com>
2021-06-02 17:48:09 +0300

    feat: add ts extention from task.service

---
+++ src/resources/tasks/task.service.ts
@@ -0,0 +1,24 @@
+import {ITask} from './task.model'
+
+const {TaskBD} = require('./task.memory.repository.');
+
+
+const addTask = (boardId:string, options:ITask)  =>  TaskBD.addTask( boardId,options );
+
+
+const getTasks = (boardId:string) => TaskBD.getTasks(boardId);
+
+
+const getTaskById = (boardId:string,taskId:string) => TaskBD.getTaskById(boardId,taskId);
+
+
+const updateTask = (boardId:string, taskId:string, options:ITask) =>  TaskBD.updateTask(boardId,taskId,options);
+
+
+const deleteTask = (boardId:string, taskId:ITask) => TaskBD.deleteTask(boardId, taskId);
+
+module.exports.updateTask = updateTask;
+module.exports.addTask = addTask;
+module.exports.getTasks = getTasks;
+module.exports.getTaskById = getTaskById;
+module.exports.deleteTask = deleteTask;
