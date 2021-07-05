dd8741f9d020a3d864b1f980fb06860b396f0ae5
Michail Fuodorov <mishafuod@gmail.com>
2021-07-05 15:38:29 +0300

    feat: add swagger on nest-application

---
+++ migrate-to-nest/src/main.ts
@@ -0,0 +1,15 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+const swaggerUI = require('swagger-ui-express');
+const path = require('path' );
+const YAML = require('yamljs');
+
+async function bootstrap() {
+  const swaggerDocument = YAML.load(path.join(__dirname, '../doc/api.yaml'));
+
+  const PORT = process.env.PORT || 4000;
+  const app = await NestFactory.create(AppModule);
+  app.use('/doc', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+  await app.listen(PORT, () => process.stdout.write(`App is running on port:${PORT}\n`));
+}
+bootstrap();
---
+++ migrate-to-nest/src/users/users.controller.ts
@@ -0,0 +1,91 @@
+import { Controller, Get, Post, Body, Param, Delete, Put } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.usersService.create(createUserDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.usersService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.usersService.findOne(+id);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return this.usersService.update(+id, updateUserDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.usersService.remove(+id);
+  }
+}
+
+
+/* router.route('/').post(async (req:Request, res:Response, next: NextFunction) => {
+  try{
+    const result = await createNewUser (entityManager, req.body);
+    res.status(201).json(result);
+  } catch (err) {
+    next(err)
+  }
+});
+
+
+router.route('/').get(async (_req:Request, res:Response, next: NextFunction) => {
+try {
+  const users = await getAllUsers(entityManager);
+  res.json(users);
+}
+catch (err) {
+  next(err)
+}
+});
+
+router.route('/:userId').delete(async (req:Request, res:Response, next: NextFunction) => {
+  try{
+  const {userId} = req.params;
+  const result = await deleteUserById(entityManager, userId);
+  if (result === "OK") {
+    res
+    .status(204)
+    .send("The user has been deleted")
+  }
+ } catch(err){
+ next(err);
+}
+});
+
+router.route('/:userId').put(async (req:Request, res:Response, next: NextFunction) => {
+try {
+  const {userId} = req.params;
+  const result = await updateUserById(entityManager, userId, req.body);
+  res.json(result)
+} catch (err) {
+  next(err);
+}
+});
+
+router.route('/:userId').get(async (req:Request, res:Response, next: NextFunction) => {
+try{
+const {userId} = req.params;
+const result = await getUserById(entityManager, userId);
+res.json(result);
+} catch (err){
+ next(err);
+}
+});
+
+}) */
